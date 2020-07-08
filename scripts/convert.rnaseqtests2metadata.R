20c0692e574c55ec5e7d6bdf0daf6e13d130ee7a
Renzo Balboa <renzobalboa@gmail.com>
2020-07-08 15:30:14 +1000

    update pacific summary md script

--- scripts/convert.rnaseqtests2metadata.R
+++ scripts/convert.rnaseqtests2metadata.R
@@ -9,21 +9,23 @@ library(tidyverse)
 dat <- read_tsv("pacific/figures/rnaseqtests.txt", col_names=T)
 colnames(dat) <- c("filename", "class", "preads", "pprop", "p95reads", "p95prop")
 
+#Separate above and below thresholds
 tmp1 <- dat %>%
   select(filename,class, preads, pprop) %>%
   mutate(threshold_0.95="below") %>%
   select(filename,class, threshold_0.95, preads, pprop) %>%
-  setNames(c("filename","class","pp_0.95","reads","prop_totalreads"))
+  setNames(c("SRA_ID","class","pp_0.95","reads","prop_totalreads"))
 
 tmp2 <- dat %>%
   select(filename,class, p95reads, p95prop) %>%
   mutate(threshold_0.95="above") %>%
   select(filename,class, threshold_0.95, p95reads, p95prop) %>%
-  setNames(c("filename","class","pp_0.95","reads","prop_totalreads"))
+  setNames(c("SRA_ID","class","pp_0.95","reads","prop_totalreads"))
 
+#Concatenate files and remove file extensions
 combined.df <- rbind(tmp2,tmp1)
-combined.df$filename <- gsub("\\.fa.*","",combined.df$filename)
+combined.df$SRA_ID <- gsub("\\.fa.*","",combined.df$SRA_ID)
 
-write_delim(combined.df, "pacific/metadata/pacificsummaries.realdatasets.txt")
+write_tsv(combined.df, "pacific/metadata/pacificsummaries.realdatasets.txt")
 
 
